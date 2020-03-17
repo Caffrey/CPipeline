6fda33e1578ebd9b0227249b2f6e61d9c88b7a97
Caffrey <447165738@qq.com>
2020-03-17 23:24:13 +0800

    update

---
+++ Forward/Assets/CPipeline/Runtime/CPipelineAsset.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CPipeline.Runtime
+{
+    [CreateAssetMenu(menuName = "Rendering/My Pipeline")]
+    public class CPipelineAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return new CRenderingPipeline();
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+        }
+    }
+}
+
+
+
+
---
+++ Forward/Assets/CPipeline/Runtime/CRenderingPipeline.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CPipeline.Runtime
+{
+
+    public class CRenderingPipeline : RenderPipeline
+    {
+
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            BeginFrameRendering(context,cameras);
+
+            BeginCameraRendering(context, cameras[0]);
+
+            context.SetupCameraProperties(cameras[0]);
+            context.DrawSkybox(cameras[0]);
+            context.Submit();
+
+            EndCameraRendering(context, cameras[0]);
+
+
+            EndFrameRendering(context, cameras);
+        }
+
+    }
+}
+
+
