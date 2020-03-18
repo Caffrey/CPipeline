9340110b4da50f03df33584258e4a5268b8cd633
447165738@qq.com <447165738@qq.com>
2020-03-18 19:34:52 +0800

    提交 光照渲染

--- Forward/Assets/CPipeline/Runtime/CForward/CRenderingPipeline.cs
+++ Forward/Assets/CPipeline/Runtime/CForward/CRenderingPipeline.cs
@@ -1,22 +1,29 @@
-﻿using UnityEngine;
+﻿using Unity.Collections;
+using UnityEngine;
 using UnityEngine.Rendering;
 
 namespace CPipeline.Runtime
 {
+
+
+
     //TODO 
     /*
-     * 0.RENDERING UNLIT
-     * 0.RENDERING LIT
+     * 0.RENDERING UNLIT  OK
+     * 0.RENDERING LIT 
      * 
-     * 1.RENDERING OP
-     * 2.RENDERING TRANSPARENT
+     * 1.RENDERING OP OK 
+     * 2.RENDERING TRANSPARENT OK
      * 3.BASIC RENDERING SHADER LIB
      * 4.PBR RENDERING LIB
      * 
      */
     public class CRenderingPipeline : RenderPipeline
     {
-
+        CommandBuffer cmd = new CommandBuffer
+        {
+            name = "SRP"
+        };
         protected override void Render(ScriptableRenderContext context, Camera[] cameras)
         {
            BeginFrameRendering(context,cameras);
@@ -29,11 +36,18 @@ namespace CPipeline.Runtime
             EndFrameRendering(context, cameras);
         }
 
-        static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+        static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlits");
 
         protected void RenderCamera(ScriptableRenderContext context, Camera camera)
         {
             BeginCameraRendering(context,camera);
+            if(camera.cameraType == CameraType.SceneView)
+            {
+                ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+            }
+            cmd.Clear();
+            cmd.ClearRenderTarget(true, true, Color.clear);
+            context.ExecuteCommandBuffer(cmd);
 
             //setup camera
             context.SetupCameraProperties(camera);
@@ -41,36 +55,41 @@ namespace CPipeline.Runtime
             //draw skybox
             context.DrawSkybox(camera);
 
-
             //culling
             ScriptableCullingParameters cullingParams;
             camera.TryGetCullingParameters(out cullingParams);
 
             var cullResult = context.Cull(ref cullingParams);
 
+            //setup light
+            SetupLight(ref cullResult);
 
 
-            //draw geo
 
-            SortingSettings sortSetting = new SortingSettings(camera);
-            DrawingSettings drawSetting = new DrawingSettings(unlitShaderTagId, sortSetting);
-            FilteringSettings filteringSeting = new FilteringSettings(RenderQueueRange.all);
+            //draw opaque
+
+            SortingSettings opaqueSortSetting = new SortingSettings(camera)
+            {
+                criteria = SortingCriteria.CommonOpaque
+            };
+
+            DrawingSettings opaqueDrawSetting = new DrawingSettings(unlitShaderTagId, opaqueSortSetting);
+            FilteringSettings opaqueFilteringSeting = new FilteringSettings(RenderQueueRange.opaque);
+
+            context.DrawRenderers(cullResult, ref opaqueDrawSetting, ref opaqueFilteringSeting);
 
 
-            context.DrawRenderers(cullResult, ref drawSetting, ref filteringSeting);
-            
-            
+            //draw transparent
+            SortingSettings transparentSortSetting = new SortingSettings(camera)
+            {
+                criteria = SortingCriteria.CommonTransparent
+            };
 
-
+            DrawingSettings transparentDrawSetting = new DrawingSettings(unlitShaderTagId, transparentSortSetting);
+            FilteringSettings transparentFilteringSeting = new FilteringSettings(RenderQueueRange.transparent);
+            context.DrawRenderers(cullResult, ref transparentDrawSetting, ref transparentFilteringSeting);
 
             //render unlit
-
-
-
-
-
-
-
 
 
             context.Submit();
@@ -78,6 +97,26 @@ namespace CPipeline.Runtime
             EndCameraRendering(context,camera);
         }
 
+        public void SetupLight(ref CullingResults cullResults)
+        {
+
+            NativeArray<VisibleLight> lights = cullResults.visibleLights;
+
+            Vector4[] lightColors = new Vector4[lights.Length];
+            Vector4[] lightDirection = new Vector4[lights.Length];
+
+           
+
+            cmd.SetGlobalFloat("_lightLength", lights.Length);
+            for(int i = 0; i < lights.Length; i++)
+            {
+                lightColors[i] = lights[i].finalColor;
+                lightDirection[i] = lights[i].localToWorldMatrix.GetColumn(2);
+            }
+           
+            cmd.SetGlobalVectorArray("_lightColors", lightColors);
+            cmd.SetGlobalVectorArray("_lightDirection", lightDirection);
+        }
 
     }
 }
